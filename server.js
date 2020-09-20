61b117464e6c98cf53283e05b50a91a27b47217c
aavesh1 <aaveshd.ic.17@nsit.net.in>
2020-09-20 06:28:47 +0530

    First local to remote commit

---
+++ server.js
@@ -0,0 +1,62 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import Cards from './dbcards.js'
+import Cors from 'cors'
+    //App Config
+const app = express();
+const port = process.env.PORT || 8001
+const connection_url = `mongodb+srv://aaveshtinderadmin:2MjSsjNAKTu7CY1f@cluster0.eph5a.mongodb.net/tinderdb?retryWrites=true&w=majority`
+    //Middlewares
+app.use(express.json())
+app.use(Cors())    
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs');
+//DB COnfig
+ mongoose.connect(connection_url , {
+     useNewUrlParser: true ,
+     useCreateIndex: true , 
+     useUnifiedTopology: true , 
+ })   
+
+    //API Endpoints
+    app.get('/', (req , res)=> res.status(200).send('Hello World'))
+    
+    
+    app.get('/createcard' , (req,res)=>{
+            res.render('form.ejs')
+    })
+
+
+    app.post('/tinder/card' , (req , res)=>{
+      const name = req.body.name ;
+        const imgUrl = req.body.url ;
+       Cards.create({name:name , imgUrl:imgUrl} , (err , data)=>{
+           if (err) {
+               console.log(err)
+           }
+           else {
+               console.log(name)
+               res.send(data)
+           }
+       } )
+    });
+
+    app.get('/tinder/card' , (req,res)=>{
+
+      
+
+        Cards.find((err , data)=>{
+            if (err) {
+                res.status(500).send(err)
+            }
+            else {
+                res.status(200).send(data)
+            }}
+            )
+    })
+
+
+    //Listener
+    app.listen(port , ()=> console.log(`listening on ${port}`));
